96520e4e43eabee2a254d0f14e95be2218266f80
Tarun Raghav <120802593+TarunRaghav369@users.noreply.github.com>
2024-11-29 06:06:03 +0000

    add product page

--- my-react-app/src/components/App.jsx
+++ my-react-app/src/components/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import Products from './Products';
 
 const AppRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/products" element={<Products />} />
 
       </Routes>
     </Router>
